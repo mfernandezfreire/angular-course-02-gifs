20ebeb97e6069675b36f5148847da6eb372f1c90
mfernandezfreire <mfernandezfreir.ext@acciona.com>
2022-08-30 13:29:03 +0200

    firsts steps

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  // The '!' symbol means Non-null assertion operator
+  // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#non-null-assertion-operator
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef;
+
+  buscar(value: string) {
+    console.log(this.txtBuscar);
+  }
+
+}
